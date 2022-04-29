7f69dea37f018e933bbcd063125688418dee423b
roqu1 <snipklas@gmail.com>
2022-04-29 11:17:55 +0200

    3 files added

---
+++ src/app/mitte/mitte.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mitte',
+  templateUrl: './mitte.component.html',
+  styleUrls: ['./mitte.component.css']
+})
+export class MitteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
