124de1912c48df78d997320455632348b69976b6
MangutHD <74400412+MangutHD@users.noreply.github.com>
2022-04-29 11:19:40 +0200

    Update mitte.component.ts

--- src/app/mitte/mitte.component.ts
+++ src/app/mitte/mitte.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Arbeitstellen } from '../counter/Arbeitstellen';
 
 @Component({
   selector: 'app-mitte',
@@ -7,8 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MitteComponent implements OnInit {
 
+  
+  x : number 
   constructor() { }
 
+
+  ausblenden() {
+
+    document.getElementById("1").style.visibility = "hidden";
+    document.getElementById("2").style.visibility = "hidden";
+    document.getElementById("3").style.visibility = "hidden";
+    
+    };
+    
+    einblenden(){
+      
+      let text = this.x.toString();
+      document.getElementById(text).style.visibility = "visible";
+      this.x += 1
+      
+    }
   ngOnInit() {
   }
 
