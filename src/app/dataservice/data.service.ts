8c6485ee317ac552ecf29e6f25e1773a2b220547
MangutHD <74400412+MangutHD@users.noreply.github.com>
2022-05-06 09:03:10 +0200

    Update data.service.ts

--- src/app/dataservice/data.service.ts
+++ src/app/dataservice/data.service.ts
@@ -5,8 +5,14 @@ import { Model } from '../counter/model';
 export class DataService {
   items: Model[] = [];
 
-  constructor() {
-    this.items.push(new Model(1, 'Fischer', 0));
+   this.items.push(new Model(1, 'Fischer', 0));
     this.items.push(new Model(2, 'Feld', 0));
+    this.arbeit = { "feldanzahl": 0,
+      "fischeranzahl": 0, "holzfaeller" : 0, "mine" : 0, "oel" : 0 
+    }
   }
+
+ get arbeitstellen(){
+   return this.arbeit
+ }
 }
