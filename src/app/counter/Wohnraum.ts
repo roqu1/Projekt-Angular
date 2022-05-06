e4d2e5d925e43964bb97d4277b3d489ff8f624e8
roqu1 <snipklas@gmail.com>
2022-05-06 10:00:46 +0200

    2 files modified

--- src/app/counter/Wohnraum.ts
+++ src/app/counter/Wohnraum.ts
@@ -1,3 +1,3 @@
-export class Arbeitstellen {
+export class Wohnraum {
   constructor(public zeltanzahl: number,public bungalowanzahl:number,public hausanzahl:number,public einfamilienhausanzahl:number,public mehrfamilienhausanzahl:number) {}
 }
--- src/app/dataservice/data.service.ts
+++ src/app/dataservice/data.service.ts
@@ -1,20 +1,33 @@
 import { Injectable } from '@angular/core';
 import { Arbeitstellen } from '../counter/Arbeitstellen';
 import { Model } from '../counter/model';
+import { Wohnraum } from '../counter/Wohnraum';
 
 @Injectable()
 export class DataService {
   items: Model[] = [];
   private arbeit : Arbeitstellen;
+  private wohn : Wohnraum;
   constructor() {
    this.items.push(new Model(1, 'Fischer', 0));
     this.items.push(new Model(2, 'Feld', 0));
     this.arbeit = { "feldanzahl": 0,
       "fischeranzahl": 0, "holzfaeller" : 0, "mine" : 0, "oel" : 0 
     }
+    this.wohn = {
+      "zeltanzahl": 0,
+      "bungalowanzahl":0,
+      "hausanzahl":0, 
+      "einfamilienhausanzahl":0,
+      "mehrfamilienhausanzahl":0
+    }
   }
 
  get arbeitstellen(){
    return this.arbeit
+ }
+
+ get wohnraum() {
+   return this.wohnraum;
  }
 }
