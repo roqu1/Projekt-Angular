bc0bd0eb2ff262807b09e5d133fd3c7ab45fe966
MangutHD <74400412+MangutHD@users.noreply.github.com>
2022-05-06 09:08:21 +0200

    Update data.service.ts

--- src/app/dataservice/data.service.ts
+++ src/app/dataservice/data.service.ts
@@ -4,6 +4,7 @@ import { Model } from '../counter/model';
 @Injectable()
 export class DataService {
   items: Model[] = [];
+  private arbeit : Arbeitstellen;
 
    this.items.push(new Model(1, 'Fischer', 0));
     this.items.push(new Model(2, 'Feld', 0));
