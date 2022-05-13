4ebc332bd5417a75ead7d80cba13df525e785617
roqu1 <snipklas@gmail.com>
2022-05-13 08:55:54 +0200

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,8 @@ import { HelloComponent } from './hello.component';
 import { Test2Component } from './test2/test2.component';
 import { MitteComponent } from './mitte/mitte.component';
 import { DataService } from './dataservice/data.service';
-import { ErdeComponent } from './erde/erde/erde.component';
+import { ErdeComponent } from './erde/erde.component';
+
 
 
 @NgModule({
--- src/app/erde/erde.component.ts
+++ src/app/erde/erde.component.ts
@@ -2,12 +2,11 @@ import { Component, OnInit, VERSION } from '@angular/core';
 import { DataService } from '../dataservice/data.service';
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  selector: 'my-erde',
+  templateUrl: './erde.component.html',
+  styleUrls: ['./erde.component.css'],
 })
-export class AppComponent implements OnInit {
-  name = 'Angular ' + VERSION.major;
+export class ErdeComponent implements OnInit {
 
   cookiecounter: number;
   isintervallRunning: boolean = false;
