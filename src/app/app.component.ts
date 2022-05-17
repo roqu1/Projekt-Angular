a3467cd4f96af151b8ccb3ba1196b3d9385951b7
roqu1 <snipklas@gmail.com>
2022-05-17 09:54:03 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,24 +7,18 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
-  againfunction:boolean = true;
-  boolean:boolean=false
   
   timer = setInterval(() => {
-    if(this.againfunction==true) {
     this.earthrandom();
-  }
   }, 5000);
 
 earthrandom() {
-  this.againfunction=false;
   const earthrandom = (document.getElementById('randomcookie') as HTMLInputElement);
   const horizontal = Math.floor(Math.random() * 1000) + 1;
   const vertical = Math.floor(Math.random() * 700) + 1;
   earthrandom.style.marginLeft = horizontal+"px";
   earthrandom.style.marginTop = vertical+"px";
-
-   earthrandom.style.display = "initial";
+  earthrandom.style.display = "initial";
   earthrandom.style.display = "none";
   
 }
